2c1b9d08fcf09f75fd2d5e871fa97c8dcb8e8819
Camila <camila.abrantes@ee.ufcg.edu.br>
2021-08-22 18:05:00 -0300

    Commit 02: Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department department = new Department(1, "Book");
-		System.out.println(department);
+		
+		Seller seller = new Seller(1, "Bob", "bob@gmail", new Date(), 3000.00, department);
+		System.out.println(seller);
 
 	}
 
